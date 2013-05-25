315ade497b6f6ddbe05c2c0de11e7331a3c6fb65
jhalak <jhalak1983@yahoo.com>
2013-05-25 16:56:45 +0600

    DB initial snapshoot added with schema

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 25, 2013 at 04:55 PM
+-- Generation Time: May 25, 2013 at 02:43 PM
 -- Server version: 5.1.69-0ubuntu0.11.10.1
 -- PHP Version: 5.3.6-13ubuntu3.10
 
@@ -35,7 +35,17 @@ CREATE TABLE IF NOT EXISTS `costs` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `costs`
+--
+
+INSERT INTO `costs` (`id`, `amount`, `description`, `tour_id`, `created`, `modified`) VALUES
+(1, 2000, '', 2, '2013-05-20 18:50:45', '2013-05-20 18:50:45'),
+(2, 5000, '', 3, '2013-05-21 15:57:37', '2013-05-21 15:57:37'),
+(3, 2000, '', 5, '2013-05-21 18:03:40', '2013-05-21 18:03:40'),
+(4, 27, '', 1, '2013-05-25 14:26:39', '2013-05-25 14:26:39');
 
 -- --------------------------------------------------------
 
@@ -53,7 +63,17 @@ CREATE TABLE IF NOT EXISTS `deposits` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `deposits`
+--
+
+INSERT INTO `deposits` (`id`, `amount`, `description`, `tour_id`, `member_id`, `created`, `modified`) VALUES
+(1, 1000, '', 2, 3, '2013-05-20 18:50:54', '2013-05-20 18:50:54'),
+(2, 2000, '', 3, 6, '2013-05-21 15:57:49', '2013-05-21 15:57:49'),
+(3, 4000, '', 3, 7, '2013-05-21 15:58:02', '2013-05-21 15:58:02'),
+(4, 1000, '', 5, 10, '2013-05-21 18:03:49', '2013-05-21 18:03:49');
 
 -- --------------------------------------------------------
 
@@ -75,8 +95,8 @@ CREATE TABLE IF NOT EXISTS `groups` (
 --
 
 INSERT INTO `groups` (`id`, `name`, `created`, `modified`) VALUES
-(1, 'Admin', '2013-05-25 16:46:53', '2013-05-25 16:46:53'),
-(2, 'User', '2013-05-25 16:46:53', '2013-05-25 16:46:53');
+(1, 'Admin', '2013-05-05 18:15:05', '2013-05-05 18:15:05'),
+(2, 'User', '2013-05-05 18:17:23', '2013-05-05 18:17:23');
 
 -- --------------------------------------------------------
 
@@ -90,14 +110,21 @@ CREATE TABLE IF NOT EXISTS `members` (
   `name` varchar(50) NOT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 --
 -- Dumping data for table `members`
 --
 
 INSERT INTO `members` (`id`, `name`, `user_id`) VALUES
-(1, 'Admin', 1);
+(1, 'Test member 1', 1),
+(2, 'Test member 2', 1),
+(8, 'Test member_2', 14),
+(6, 'Tuntuli', 14),
+(7, 'Sakib', 14),
+(9, 'Test member_2', 15),
+(10, 'iMunshi', 15),
+(11, 'Jhalak', 1);
 
 -- --------------------------------------------------------
 
@@ -111,7 +138,20 @@ CREATE TABLE IF NOT EXISTS `members_tours` (
   `member_id` int(11) NOT NULL,
   `tour_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `members_tours`
+--
+
+INSERT INTO `members_tours` (`id`, `member_id`, `tour_id`) VALUES
+(1, 1, 1),
+(2, 2, 1),
+(4, 6, 3),
+(5, 7, 3),
+(6, 8, 4),
+(7, 9, 5),
+(8, 10, 5);
 
 -- --------------------------------------------------------
 
@@ -126,7 +166,18 @@ CREATE TABLE IF NOT EXISTS `tours` (
   `description` text,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `tours`
+--
+
+INSERT INTO `tours` (`id`, `name`, `description`, `user_id`) VALUES
+(1, 'Rangamati', 'efasdfasdf', 1),
+(2, 'Bogura', 'sadf asdf ', 8),
+(3, 'Hatirjhil', 'Janina ', 14),
+(4, 'Rajshahi', '', 14),
+(5, 'Rajshahi', 'asdf asdf', 15);
 
 -- --------------------------------------------------------
 
@@ -145,14 +196,18 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `username`, `password`, `group_id`, `created`, `modified`) VALUES
-(1, 'Admin', '', 'jhalak', '48cbc3ec8fd8837ea0dbdc27aef23cba9d4a91b7', 1, '2013-05-25 16:46:54', '2013-05-25 16:46:54');
+(1, 'Jhalak', 'jhalak@tasawr.com', 'jhalak', '48cbc3ec8fd8837ea0dbdc27aef23cba9d4a91b7', 1, '2013-05-05 18:37:48', '2013-05-25 14:41:05'),
+(15, 'iMunshi', 'munshi@tasawr.com', 'imunshi', '3cbad36ce94d176880a7cb2d2b32bb182be4ae3e', 2, '2013-05-21 18:02:36', '2013-05-21 18:02:36'),
+(13, 'Testy', 'test3@tasawr.com', 'testy', '18610a60c03f75e6a86af1d82604ef207f99983a', 2, '2013-05-21 14:12:29', '2013-05-21 14:12:29'),
+(14, 'Sakib', 'sakib@tasawr.com', 'sakib', '18610a60c03f75e6a86af1d82604ef207f99983a', 2, '2013-05-21 15:55:41', '2013-05-21 15:55:41'),
+(12, 'Test', 'test@tasawr.com', 'test', '18610a60c03f75e6a86af1d82604ef207f99983a', 2, '2013-05-21 14:10:35', '2013-05-21 14:10:35');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
